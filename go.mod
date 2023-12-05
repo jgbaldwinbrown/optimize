5928a9876a103e9b184177b6c15f350b44824406
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2023-12-04 17:18:01 -0700

    working mse

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module local/jgbaldwinbrown/optimize
 
 go 1.20
+
+require github.com/jgbaldwinbrown/iter v0.1.5
+
+require golang.org/x/sync v0.5.0 // indirect
---
+++ mse.go
@@ -0,0 +1,54 @@
+package optimize
+
+import (
+	"github.com/jgbaldwinbrown/iter"
+)
+
+type MSESummer struct {
+	Sum float64
+	Count float64
+}
+
+func (m *MSESummer) Update(y1, y2 float64) {
+	m.Sum += (y1 - y2) * (y1 - y2)
+}
+
+func (m *MSESummer) MeanSquaredError() float64 {
+	return m.Sum / m.Count
+}
+
+func (m *MSESummer) Reset() {
+	m.Sum = 0
+	m.Count = 0
+}
+
+func NewMseSummer() *MSESummer {
+	return &MSESummer{}
+}
+
+type Pair struct {
+	Y1 float64
+	Y2 float64
+}
+
+func (m *MSESummer) IterMSE(it iter.Iter[Pair]) (float64, error) {
+	m.Reset()
+	e := it.Iterate(func(p Pair) error {
+		m.Update(p.Y1, p.Y2)
+		return nil
+	})
+	return m.MeanSquaredError(), e
+}
+
+type IOPair struct {
+	In float64
+	Out float64
+}
+
+func FuncPair(f func(float64) float64, pairs iter.Iter[IOPair]) *iter.Iterator[Pair] {
+	return &iter.Iterator[Pair]{Iteratef: func(yield func(Pair) error) error {
+		return pairs.Iterate(func(ip IOPair) error {
+			return yield(Pair{f(ip.In), ip.Out})
+		})
+	}}
+}
